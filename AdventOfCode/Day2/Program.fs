97813884d38a60274f827a1ebeac453a0a5fa5c5
Richie Jang <richiejang@naver.com>
2015-12-22 13:05:22 +0900

    Solve Until Day3; 2015-12-22

---
+++ AdventOfCode/Day2/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+open System.IO
+open System.Net
+open System.Collections.Generic
+
+type Paper = {l:int; w:int; h:int}
+
+let inputfile = "../../input.txt"
+
+let papers = new List<Paper>()
+
+let parse (l: string) =
+    let v = l.Split ('x') |> Seq.map (fun i -> (int i)) |> Seq.sort
+    {l = (v |> Seq.item 0); w = (v |> Seq.item 1); h = (v |> Seq.item 2)}
+
+File.ReadLines(inputfile)
+|> Seq.iter (fun i -> 
+                let p = (parse i)
+                papers.Add(p))
+
+Console.WriteLine ("Loading Finished : {0}", papers.Count) 
+
+let getLengthPaper (p: Paper) =
+    2 * (p.l * p.w) + 2 * (p.w * p.h) + 2 * (p.h * p.l) + (p.l * p.w)
+
+let sum = 
+    papers 
+    |> Seq.map getLengthPaper
+    |> Seq.sum
+
+printfn "Sum : %d" sum
+
+Console.ReadLine() |> ignore
---
+++ AdventOfCode/Day3/Program.fs
@@ -0,0 +1,36 @@
+﻿// day3
+open System
+open System.IO
+open System.Collections.Generic
+
+let inputfile = "../../input.txt"
+
+let pos = File.ReadAllText(inputfile)
+
+type House = 
+    struct
+        val mutable X: int
+        val mutable Y: int
+    end
+
+let mutable curHouse = new House()
+
+let santaPath = new List<House>()
+
+santaPath.Add(curHouse)
+
+for i in pos do
+    match i with
+    | '^' -> curHouse.Y <- curHouse.Y - 1
+    | '>' -> curHouse.X <- curHouse.X + 1
+    | '<' -> curHouse.X <- curHouse.X - 1
+    | 'v' -> curHouse.Y <- curHouse.Y + 1
+    | _ -> ()
+    santaPath.Add(curHouse)
+
+let setpaths = santaPath |> Seq.distinct
+
+printfn "All Paths : %d" santaPath.Count
+printfn "Result : %d" (setpaths |> Seq.length)
+
+Console.ReadLine() |> ignore
