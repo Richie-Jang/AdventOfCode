d68493c02336ad4f10e325b319961e0a9e707940
Richie Jang <richiejang@naver.com>
2015-12-22 19:47:14 +0900

    Solved Day5. 2015-12-22

--- AdventOfCode/Day5/Program.fs
+++ AdventOfCode/Day5/Program.fs
@@ -3,7 +3,7 @@ open System.IO
 
 let inputfile = "../../input.txt"
 
-let lines = File.ReadLines(inputfile)
+let lines = File.ReadLines(inputfile) |> Seq.filter (fun i -> i <> "")
 
 let checkLine1 line =
     // aeiou
@@ -12,32 +12,21 @@ let checkLine1 line =
         line 
         |> Seq.filter
             (fun i -> i = 'a' || i = 'e' || i = 'i' || i = 'o' || i = 'u')
-        |> Seq.countBy (fun i -> i)
-    //let case1count = case1 |> Seq.length
+        |> Seq.toList
+    //let distinctcase1 = case1 |> Seq.distinct |> Seq.toList
+    //let test = sprintf "%A" distinctcase1
     if (case1 |> Seq.length) >= 3 then result <- true
-    else
-        let f = case1 |> Seq.tryFind(fun i -> (snd i) >= 3)
-        if f.IsSome then result <- true
     result
 
 let checkLine2 (line: string) =
     // twice in a row
-    let counter = ref 0
-    let doubleCount = 
-        line
-        |> Seq.take(line.Length-1)
-        |> Seq.filter
-            (fun i -> 
-                let c1 = i
-                let c2 = line.[!counter+1]
-                incr counter
-                c1 = c2
-            )
-        |> Seq.length
-    if doubleCount > 0 then true else false
-
-    
-
+    let result = ref false
+    for i = 0 to line.Length-2 do
+        let c1 = line.[i]
+        let c2 = line.[i+1]
+        if c1 = c2 then result := true
+    !result
+   
 let checkNiceLine3 (niceline: string) =
     // not contain ab cd pq xy
     match niceline with
@@ -47,22 +36,52 @@ let checkNiceLine3 (niceline: string) =
     | a when a.Contains ("xy") -> false
     | _ -> true
 
-//// test Code
-//let checkString str =
-//    match (checkNiceLine3 str) with
-//    | true -> 
-//        (checkLine1 str) && (checkLine2 str)
-//    | false -> false
+// test Code
+let checkString str =
+    match (checkNiceLine3 str) with
+    | true -> 
+        (checkLine1 str) && (checkLine2 str)
+    | false -> false
 
 
 // check lines
+open System.Text.RegularExpressions
+
+// three Vowels
+let checkFirst line =
+    let ms = Regex.Matches (line, ".*([aeiou].*){3,}.*")
+//    if (ms.Count > 0) then
+//        for i = 0 to ms.Count-1 do
+//            let mmm = ms.Item i
+//            for j = 0 to mmm.Groups.Count-1 do
+//                let mmmm = mmm.Groups.Item j
+//                printfn "[%d] %s" j mmmm.Value
+    ms.Count > 0
+
+// hasDoubleLetter
+let checkSecond line =
+    let ms = Regex.Matches (line, ".*(.)\\1.*")
+    ms.Count > 0
+
+let checkThird line =
+    let ms = Regex.Matches (line, ".*(ab|cd|pq|xy)+.*")
+    ms.Count = 0
+
+let findNice1 =
+    lines
+    |> Seq.filter(fun i -> (checkLine1 i) && (checkLine2 i) && (checkNiceLine3 i))
+    |> Seq.toList
 
 let findNice = 
     lines
-    |> Seq.filter(fun i -> checkNiceLine3 i)
-    |> Seq.filter(fun i -> (checkLine1 i) && (checkLine2 i))
-    |> Seq.length
+    //|> Seq.filter(fun i -> (checkLine1 i) && (checkLine2 i) && (checkNiceLine3 i))
+    |> Seq.filter (fun i -> checkFirst i && checkSecond i && checkThird i)
+    |> Seq.toList
 
-printfn "%d" findNice
+File.WriteAllLines ("c:\\Temp\\test1.txt", findNice1)
+File.WriteAllLines ("c:\\Temp\\test2.txt", findNice)
+
+printfn "MyWay -> %d" (findNice1.Length)
+printfn "RegWay -> %d" findNice.Length
 
 Console.ReadLine() |> ignore
